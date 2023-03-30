78f7c89d67cb114d1a18bfee022854d765057bee
andychen <descartes1960s@gmail.com>
2023-03-30 13:03:27 +0800

    Set up Navbar component with Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<template><Navbar /></template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "./assets/css/main.css";
+import Navbar from "./components/Navbar.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Navbar },
+};
 </script>
 
 <style>
